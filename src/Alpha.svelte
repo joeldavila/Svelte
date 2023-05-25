32637339d0ed3e1789436a63e7a7f66a4577c55e
Joel <joel.davila88@gmail.com>
2023-05-24 21:10:58 -0400

    first commit

---
+++ src/Alpha.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let color;
+  export let value = 0;
+</script>
+
+<p>{color}({value})</p>
+<input type="range" step=".1"  max="1" bind:value />
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  input {
+    min-width: 70%;
+    max-width: 80%;
+    padding: 10px 0;
+  }
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,93 @@
+<script>
+  import Color from "./Color.svelte";
+  import Alpha from "./Alpha.svelte";
+  let redValue = 0;
+  let greenValue = 0;
+  let blueValue = 0;
+  let alpha = 1;
+  let display = "display"
+  let setDisplay = function(){
+    if (display == "flex"){
+      return
+
+    }else{
+      return "none"
+    }
+
+  }
+
+  function RGBAToHexA(r, g, b) {
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+
+    if (r.length == 1) r = "0" + r;
+    if (g.length == 1) g = "0" + g;
+    if (b.length == 1) b = "0" + b;
+
+    return "#" + r + g + b;
+  }
+</script>
+
+<div class="container">
+  <h1>Color Designer</h1>
+  <div class="container">
+    <Color color="Red" bind:value={redValue} />
+    <Color color="Green" bind:value={greenValue} />
+    <Color color="Blue" bind:value={blueValue} />
+    
+  </div>
+  <div style="display:{display}" class="container">
+    <Alpha color="Alpha" bind:value={alpha} />
+  </div>
+
+  <div
+    class="box"
+    style="background-color: rgba({redValue},{greenValue},{blueValue},{alpha});"
+  />
+  <div />
+  <p>
+    rgba({redValue},{greenValue},{blueValue},{alpha})
+  </p>
+  <span>Use Alpha value only for RGBA, for Hex# move the Alpha value to 1</span>
+
+  <p>
+    Hex Color:{RGBAToHexA(redValue, greenValue, blueValue)}
+  </p>
+</div>
+
+<style>
+  * {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  .box {
+    margin-top: 20px;
+    min-width: 70%;
+    max-width: 80%;
+
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    border-radius: 20px;
+  }
+  p {
+    color: lightslategray;
+    font-size: 35px;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  span {
+    font-size: 10px;
+    position: relative;
+    top: -30px;
+    color: red;
+  }
+</style>
